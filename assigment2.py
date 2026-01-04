39a0ca28e66ff0d7ead2a702b7ca5dec9c2eaa8a
“aysstsn” <ayse.tosun2@std.yeditepe.edu.tr>
2026-01-04 22:07:42 +0300

    add assigment

---
+++ assigment2.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+def stat(): 
+    #load the data
+    
+    data = np.loadtxt("populations.txt")
+    
+    #extract hare population
+    
+    hare = data[:, 1]
+    
+    #year of minimum hare population
+    
+    min_index = np.argmin(hare)
+    min_year_hare = data[min_index, 0]
+    
+    #lynx population average
+    
+    lynx = data[:, 2]
+    lynx_avg = np.mean(lynx)
+    
+    #with sum of species create new_data
+    
+    species_sum = np.sum(data[:, 1:], axis = 1)
+    new_data = np.column_stack((data, species_sum))
+    
+    #make the carrot population below 4000 equal to 0
+    
+    new_data[new_data[:, 3] < 4000, 3] = 0
+    
+    #return results
+    
+    return data, hare, min_year_hare, lynx_avg, new_data
