77f3608f83311905a9739070f4002a16d4b41c8c
“aysstsn” <ayse.tosun2@std.yeditepe.edu.tr>
2026-01-04 22:53:57 +0300

    add assignment2

--- assignment2.py
+++ assignment2.py
@@ -2,33 +2,30 @@ import numpy as np
 
 def stat(): 
     #load the data
-    
     data = np.loadtxt("populations.txt")
     
     #extract hare population
-    
     hare = data[:, 1]
     
     #year of minimum hare population
-    
     min_index = np.argmin(hare)
     min_year_hare = data[min_index, 0]
     
     #lynx population average
-    
     lynx = data[:, 2]
     lynx_avg = np.mean(lynx)
     
-    #make new_data by adding sum of species
-    
+    #make sum from original data 
     species_sum = np.sum(data[:, 1:], axis=1)
-    new_data = np.column_stack((data, species_sum))
+    
+    #create ne_data as a copy
+    new_data = data.copy()
     
     #modify carrot population below 4000 to 0
-    
     new_data[new_data[:, 3] < 4000, 3] = 0
     
+    #append original sum column
+    new_data = np.column_stack((data, species_sum))
     
     #return results
-    
     return data, hare, min_year_hare, lynx_avg, new_data
